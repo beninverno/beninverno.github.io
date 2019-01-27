ba02e7ad1b0a0f15f90e688cba704e12db826330
beninverno <benjamin.inverno@gmail.com>
2019-01-27 20:12:38 +1100

    added all files

---
+++ journeys/colour/blue.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+    populatewithcolours();
+}
+
+function populatewithcolours() {
+    var colorArray = ['#e83737','#e85436','#e86535','#e87935','#e89d35','#e8c435','#e8df35','#c1e835','#a3e835','#79e835','#58e835','#3de835','#35e840','#35e85b','#35e870','#35e882','#35e8a3','#35e8bb','#35e8cd','#35e8df','#35dce8','#35cae8','#35ace8','#3594e8','#3579e8','#355be8','#3535e8','#5835e8','#7635e8','#ac35e8','#cd35e8','#e835e5','#e835b5','#e8359a','#e83576','#e83546']
+    for(i = 0; i < 36; i++) {
+        var currentDiv = document.getElementsByClassName("column")[i];
+        currentDiv.style.backgroundColor = colorArray[i];
+        if (i != 28) {
+            currentDiv.addEventListener("click", function(){this.classList.add('fade');
+                                                            this.style.backgroundColor = "#9ba9c1";
+                                                        })
+        }
+        else if (i == 28) {
+            currentDiv.addEventListener("click", function() {hideall()});
+        }
+    }
+}
+
+function hideall() {
+    var header = document.getElementsByTagName("h1")[0];
+    header.innerHTML = "That is the correct colour!"
+    for(i = 0; i < 36; i++) {
+        var currentDiv = document.getElementsByClassName("column")[i];
+        currentDiv.classList.add('fade');
+        currentDiv.style.backgroundColor = '#9ba9c1';
+        setTimeout(function(){currentDiv.classList.add('hider')}, 2000);
+    setTimeout(function(){
+        for(i = 0; i < 6; i++) {
+            document.getElementsByClassName("row")[i].classList.add("hider")
+        }}, 2000);
+    setTimeout(function(){header.innerHTML = "That wasn't too hard, was it? You did great!"}, 2500);
+    setTimeout(function(){header.innerHTML = "Let's try again with some more, why don't we? Are you up for the challenge?"}, 6000);
+    setTimeout(function(){document.getElementById("yesorno").classList.remove("hider")}, 7000);
+    }
+}
---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    main();
+}
+function main() {
+    var titleArray = ["titles/bensworld1.gif", "titles/bensworld2.gif", "titles/bensworld3.png", "titles/bensworld4.png", "titles/bensworld5.png", "titles/bensworld6.gif", "titles/bensworld7.png", "titles/bensworld8.png", "titles/bensworld9.gif", "titles/bensworld10.png", "titles/bensworld11.png", "titles/bensworld12.png", "titles/bensworld13.png", "titles/bensworld14.png", "titles/bensworld15.png", "titles/bensworld16.png"];
+    var imgArray = ["pictures/couple1.gif", "pictures/cover.jpg", "pictures/coverblue.jpg", "pictures/coverRED.jpg", "pictures/duck.jpg", "pictures/rose.jpg", "pictures/sleepincolor.jpg", "pictures/vertigo.jpg", "pictures/weeson.jpg"];
+    var titleNum = Math.floor((Math.random() * titleArray.length));
+    var imgNum = Math.floor((Math.random() * imgArray.length));
+    document.getElementById("header").src = titleArray[titleNum];
+    document.getElementById("picture").src = imgArray[imgNum];
+    console.log(titleNum);
+    console.log(imgNum);
+}
+
+function gotosite() {
+    window.location = document.getElementsByTagName("select")[0].value;
+}
